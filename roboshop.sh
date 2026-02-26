98e3af575eea43c2525678da882f715b1a9b7cea
Ravikumar <ravikumarjupudi143@gmail.com>
2026-02-26 10:28:32 +0530

    shell-roboshop

--- roboshop.sh
+++ roboshop.sh
@@ -2,6 +2,8 @@
 
 AMI_ID="ami-0220d79f3f480ecf5"
 SG_ID="sg-08225e8104187ede6"  # replace with your SG ID
+ZONE_ID="Z05253063NTXEVLHB6T97" #replace with your ID
+DOMAIN_NAME="bsws86s.shop"
 
 for instance in $@
 do  
@@ -10,9 +12,30 @@ do
       #get private IP
     if [ $instance != "frontend" ]; then 
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
+        RECORD_NAME="$instance.$DOMAIN_NAME" # mongodb.baws86s.shop
 
     else
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
+        RECORD_NAME="$DOMAIN_NAME" # baws86s.shop
     fi
     echo "$instace: $IP"
+
+    aws route53 change-resource-record-sets \
+    --hosted-zone-id Z05253063NTXEVLHB6T97 \
+    --change-batch '
+  {
+    "Comment": "updating record set"
+    ,"Changes": [{
+      "Action"              : "UPSERT"
+      ,"ResourceRecordSet"  : {
+        "Name"              : "'$RECORD_NAME'"
+        ,"Type"             : "A"
+        ,"TTL"              : 1
+        ,"ResourceRecords"  : [{
+            "Value"         : "'$IP'"
+        }]
+      }
+    }]
+  }
+  '
 done
